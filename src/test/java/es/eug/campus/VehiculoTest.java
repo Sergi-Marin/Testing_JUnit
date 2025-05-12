efc376b18b7d0f0faa0209277482ea53a2fc0429
Noel Riart <nriartl2@gmail.com>
2025-05-12 13:33:02 +0200

    Actualización parte Reporting | vehiculo - Ej8

--- src/test/java/es/eug/campus/VehiculoTest.java
+++ src/test/java/es/eug/campus/VehiculoTest.java
@@ -5,28 +5,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class VehiculoTest {
-
+    /**
+     * Testeamos el constructor de la clase Vehiculo con valores inválidos.
+     */
     @Test
     public void testConstructorIdNegativo() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Vehiculo(-1, "Corolla", "Nuevo", 15000.0);
         });
     }
-
+/**
+     * Testeamos el constructor de la clase Vehiculo con valores inválidos.
+     */
     @Test
     public void testConstructorModeloNulo() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Vehiculo(1, null, "Nuevo", 15000.0);
         });
     }
-
+/**
+     * Testeamos el constructor de la clase Vehiculo con valores inválidos.
+     */
     @Test
     public void testConstructorEstadoVacio() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Vehiculo(1, "Corolla", "", 15000.0);
         });
     }
-
+/**
+     * Testeamos el constructor de la clase Vehiculo con valores inválidos.
+     */
     @Test
     public void testConstructorPrecioNegativo() {
         assertThrows(IllegalArgumentException.class, () -> {
